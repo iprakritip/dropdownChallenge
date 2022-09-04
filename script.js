2acf3c1bc9cff7d1471a4f303c9e54a5ddc498c9
Prakriti Pokharel <iprakritip@gmail.com>
2022-09-04 07:00:33 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// document.getElementById("bigimg").img=URL("images/image-hero-mobile.png");
+const menuIcon = document.getElementsByClassName('menuicon')[0];
+const closeIcon = document.getElementsByClassName('closemenu')[0];
+const navContent = document.getElementsByClassName('navcontent')[0];
+const mainBody = document.getElementsByTagName('body')[0];
+menuIcon.addEventListener("click", ()=>{
+
+    closeIcon.style.display = "block";
+    navContent.style.display = "block";
+mainBody.style.opacity = "1";
+
+})
+closeIcon.addEventListener("click", ()=>{
+
+    closeIcon.style.display = "none";
+    navContent.style.display = "none";
+
+})
