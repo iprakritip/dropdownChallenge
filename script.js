9cf26279a404059c92eeb5ce7061f209c9d3a9dc
Prakriti Pokharel <iprakritip@gmail.com>
2022-09-04 12:26:31 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const menuIcon = document.getElementsByClassName('menuicon')[0];
 const closeIcon = document.getElementsByClassName('closemenu')[0];
 const navContent = document.getElementsByClassName('navcontent')[0];
-const mainBody = document.getElementsByTagName('body')[0];
+const overlay = document.getElementsByClassName('overlay')[0];
 menuIcon.addEventListener("click", ()=>{
 
     closeIcon.style.display = "block";
     navContent.style.display = "block";
-mainBody.style.opacity = "1";
+overlay.style.display= "block";
+
 
 })
 closeIcon.addEventListener("click", ()=>{
 
     closeIcon.style.display = "none";
     navContent.style.display = "none";
-
+    overlay.style.display= "none";
 })
